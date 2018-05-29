4dfa782cf11380647e63954001de29654a9b3cd7
huongnguyen071723 <huongnguyen071723@gmail.com>
2018-05-29 22:13:10 +0700

    update

--- server/server.js
+++ server/server.js
@@ -4,6 +4,39 @@ var express = require('express')
   , morgan = require('morgan');
 
   
+
+  
+// configuration files
+var configServer = require('./lib/config/server');
+
+// app parameters
+var app = express();
+app.set('port', configServer.httpPort);
+app.set('port_app', configServer.appPort);
+app.use(express.static(configServer.staticFolder));
+app.use(morgan('dev'));
+
+// serve index
+require('./lib/routes').serveIndex(app, configServer.staticFolder);
+
+// HTTP server
+var server = http.createServer(app);
+server.listen(app.get('port'), function () {
+  console.log('HTTP server listening on port ' + app.get('port'));
+});
+var server0 = http.createServer(app);
+server0.listen(app.get('port_app'), function () {
+  console.log('HTTP server listening on port ' + app.get('port_app'));
+});
+
+
+// WebSocket server
+var io = require('socket.io')(server);
+io.on('connection', require('./lib/routes/socket'));
+var io0 = require('socket.io')(server0);
+io0.on('connection', require('./lib/routes/socket'));
+
+
 var bleno = require('bleno');
 var BlenoPrimaryService = bleno.PrimaryService;
 
@@ -48,35 +81,6 @@ clientAddress);
 
 console.log("Edison initiated...");
   
-  
-// configuration files
-var configServer = require('./lib/config/server');
 
-// app parameters
-var app = express();
-app.set('port', configServer.httpPort);
-app.set('port_app', configServer.appPort);
-app.use(express.static(configServer.staticFolder));
-app.use(morgan('dev'));
-
-// serve index
-require('./lib/routes').serveIndex(app, configServer.staticFolder);
-
-// HTTP server
-var server = http.createServer(app);
-server.listen(app.get('port'), function () {
-  console.log('HTTP server listening on port ' + app.get('port'));
-});
-var server0 = http.createServer(app);
-server0.listen(app.get('port_app'), function () {
-  console.log('HTTP server listening on port ' + app.get('port_app'));
-});
-
-
-// WebSocket server
-var io = require('socket.io')(server);
-io.on('connection', require('./lib/routes/socket'));
-var io0 = require('socket.io')(server0);
-io0.on('connection', require('./lib/routes/socket'));
 
 module.exports.app = app;
